0e76b3f72881a5a93fef7daf21f57a8994456238
iljagrabar14 <iljagrabar14@gmail.com>
2024-11-19 12:38:38 +0100

    New sync style

--- app/common/crud.py
+++ app/common/crud.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 
 from app.common.models import Token
 from app.common.schemas import TokenSchema
+from config import COINGECKO_API_KEY
 
 
 def get_tokens(symbol__in: str, db: Session):
@@ -15,38 +16,73 @@ def get_tokens(symbol__in: str, db: Session):
     else:
         return db.query(Token).all()
 
+def make_get_requests_to_coingecko(request_urls):
+    result = []
+    for url in request_urls:
+        response = requests.get(url)
+        result += response.json()
+    return result
+
+def get_info_for_all_coins_from_coingecko(coins_ids):
+
+    request_urls = []
+    current_url = f'https://api.coingecko.com/api/v3/coins/markets?x_cg_demo_api_key={COINGECKO_API_KEY}&vs_currency=usd&per_page=250&'
+    for coingecko_id in coins_ids:
+        if len(coingecko_id) + len(current_url) > 2048:
+            request_urls.append(current_url)
+            current_url = f'https://api.coingecko.com/api/v3/coins/markets?x_cg_demo_api_key={COINGECKO_API_KEY}&vs_currency=usd&per_page=250&ids={coingecko_id}'
+        else:
+            if 'ids' in current_url:
+                current_url += f',{coingecko_id}'
+            else:
+                current_url += f'ids={coingecko_id}'
+    request_urls.append(current_url)
+    return make_get_requests_to_coingecko(request_urls)
+
 
 def sync_tokens(db: Session):
-    response = requests.get('https://api-osmosis.imperator.co/tokens/v2/all')
-    if response.ok:
-        tokens = parse_obj_as(List[TokenSchema], response.json())
-        token_to_create = []
-        for token in tokens:
-            db_token_query = db.query(Token).filter(Token.denom == token.denom)
-            if db_token_query.first():
-                db_token_query.update(token.dict())
-            else:
-                token_to_create.append(token)
-        for token in token_to_create:
-            db_token = Token(**token.dict())
-            db.add(db_token)
-        db.commit()
-    ntrn_response = requests.get('https://develop-multichain-api.astroport.fi/router/v2/route?start=untrn&end=ibc/f082b65c88e4b6d5ef1db243cda1d331d002759e938a0f5cd3ffdc5d53b3e349&amount=1000000&chainId=neutron-1')
-    if ntrn_response.ok:
-        ntrn_response_json = ntrn_response.json()
-        neutron_data = {
-            'price': ntrn_response_json.get('amount_out')/1000000,
-            'exponent': 6,
-            'name': 'Neutron',
-            'symbol': 'NTRN',
-            'denom': 'ibc/f082b65c88e4b6d5ef1db243cda1d331d002759e938a0f5cd3ffdc5d53b3e349',
-            'display': 'ntrn'
-        }
-        ntrn_parsed = TokenSchema.parse_obj(neutron_data)
-        db_token_query = db.query(Token).filter(Token.denom == ntrn_parsed.denom)
+    response = requests.get('https://skychart.bronbro.io/v1/assets')
+    result = []
+    coingecko_ids = []
+    for symbol in set(response.json()):
+        response = requests.get(f'https://skychart.bronbro.io/v1/asset/{symbol}')
+        if response.status_code == 200 and 'coingecko_id' in response.json():
+            coin = response.json()
+            result.append({
+                'denom': next((item['denom'] for item in coin['denom_units'] if item['exponent'] == 0),
+                              'Not defined in skychart'),
+                'exponent': max([item['exponent'] for item in coin['denom_units']]),
+                'name': coin['name'],
+                'display': coin['display'],
+                'coingecko_id': coin['coingecko_id'],
+                'liquidity': 0,
+                'volume_24h': 0,
+                'volume_24h_change': 0,
+                'price_7d_change': 0
+            })
+            coingecko_ids.append(coin['coingecko_id'])
+
+    coingecko_info = get_info_for_all_coins_from_coingecko(coingecko_ids)
+    final_result = []
+    for item in result:
+        coingecko = next(
+            (coingecko_data for coingecko_data in coingecko_info if coingecko_data['id'] == item['coingecko_id']), None)
+        if coingecko:
+            item['symbol'] = coingecko['symbol']
+            item['price'] = coingecko['current_price']
+            item['price_24h_change'] = coingecko['price_change_percentage_24h']
+            item.pop('coingecko_id')
+            if item['price']:
+                final_result.append(item)
+    tokens = parse_obj_as(List[TokenSchema], final_result)
+    token_to_create = []
+    for token in tokens:
+        db_token_query = db.query(Token).filter(Token.denom == token.denom)
         if db_token_query.first():
-            db_token_query.update(ntrn_parsed.dict())
+            db_token_query.update(token.dict())
         else:
-            db_token = Token(**ntrn_parsed.dict())
-            db.add(db_token)
-        db.commit()
+            token_to_create.append(token)
+    for token in token_to_create:
+        db_token = Token(**token.dict())
+        db.add(db_token)
+    db.commit()
